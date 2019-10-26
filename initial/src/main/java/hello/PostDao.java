592730c3aff165e192b8dc2e7a7afefc0a3f91b2
Alex Lindemann <exmann94@gmail.com>
2019-10-25 21:59:24 -0500

    formatting it working

--- initial/src/main/java/hello/PostDao.java
+++ initial/src/main/java/hello/PostDao.java
@@ -1,21 +1,26 @@
 package hello;
 
-import org.springframework.stereotype.Component;
-
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.stereotype.Component;
 
 @Component
 public class PostDao {
-    private List<Post> posts = new ArrayList<>();
-    private static Integer ID_COUNTER = 0;
-    public List<Post> getPosts(){
-        return this.posts;
-    }
+  private List<Post> posts = new ArrayList<>();
+  private static Integer ID_COUNTER = 0;
 
-    public void addPost() {
-        String id = String.valueOf(ID_COUNTER++);
-        this.posts.add(Post.builder().title(("New Title " + id)).author("Author" + id).text("Body of " + id).build());
-    }
+  public List<Post> getPosts() {
+    return this.posts;
+  }
 
+  public void addPost() {
+    String id = String.valueOf(ID_COUNTER++);
+    this.posts.add(
+        Post.builder()
+            .title(("New Title " + id))
+            .author("Author" + id)
+            .text("Body of " + id)
+            .id(ID_COUNTER - 1)
+            .build());
+  }
 }
