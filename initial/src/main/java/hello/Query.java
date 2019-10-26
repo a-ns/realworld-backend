04aa00294155d7543f0561cb8d6061a5dac2d119
Alex Lindemann <exmann94@gmail.com>
2019-10-25 21:54:13 -0500

    test commit

--- initial/src/main/java/hello/Query.java
+++ initial/src/main/java/hello/Query.java
@@ -1,24 +1,17 @@
 package hello;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
-
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Component
-public class Query
+public class Query implements GraphQLQueryResolver {
 
-        implements GraphQLQueryResolver {
+  @Autowired PostDao postDao;
 
-    @Autowired
-    PostDao postDao;
+  public List<Post> getRecentPosts(Integer count, Integer offset) {
 
-    public List<Post> getRecentPosts(Integer count, Integer offset) {
-
-        return postDao.getPosts();
-    }
-
+    return postDao.getPosts();
+  }
 }
